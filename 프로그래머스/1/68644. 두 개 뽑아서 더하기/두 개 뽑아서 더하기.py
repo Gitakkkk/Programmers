7bc6a1fceabb72d59d701c38ea398bf94ca6caf9
GITAK <tak.read2@gmail.com>
2024-10-20 11:03:35 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.55 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = []
+    
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    
+    set_answer = set(answer)  # 리스트를 바로 집합으로 변환
+    sorted_answer = sorted(set_answer)  # 집합을 정렬
+    return sorted_answer
